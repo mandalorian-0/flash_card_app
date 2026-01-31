5cfdc86907fe39741ed2e24e89a0ac135204cb65
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-31 04:25:44 -0800

    feat: get random word

--- cards/utils.py
+++ cards/utils.py
@@ -1,1 +1,7 @@
-import pandas
+import random
+
+def random_word(data: list, canvas, word):
+    random_index = random.randint(0, len(data) - 1)
+    print(data[random_index]["French"])
+    canvas.itemconfig(word, text=data[random_index]["French"])
+    
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import tkinter
+import pandas as pd
 from pathlib import Path
+from functools import partial
+
+from cards import utils
 
 BACKGROUND_COLOR = "#B1DDC6"
 ROOT = Path(__file__).parent
 IMAGES = ROOT.joinpath("images")
 DATA_PATH = ROOT.joinpath("data") / "french_words.csv"
+
+# read data
+data = pd.read_csv(DATA_PATH)
+
+# transform into list
+data_list = data.to_dict("records")
 
 # window setup
 window = tkinter.Tk()
@@ -29,6 +39,9 @@ is_not_known_img = tkinter.PhotoImage(file=IMAGES.joinpath("wrong.png"))
 
 wrong_button = tkinter.Button(image=is_not_known_img, highlightthickness=0, border=0, cursor="hand2")
 right_button = tkinter.Button(image=is_known_img, highlightthickness=0, border=0, cursor="hand2")
+
+wrong_button.config(command=partial(utils.random_word, data_list, canvas, word))
+right_button.config(ccommand=partial(utils.random_word, data_list, canvas, word))
 
 wrong_button.grid(row=1, column=0)
 right_button.grid(row=1, column=1)
