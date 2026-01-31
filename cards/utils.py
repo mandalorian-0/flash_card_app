c341948468aca9b17b22f5e4dfe3f2f4e16b1687
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-31 06:10:21 -0800

    feat: add card flipping (1)

--- cards/utils.py
+++ cards/utils.py
@@ -1,7 +1,5 @@
 import random
 
-def random_word(data: list, canvas, word):
-    random_index = random.randint(0, len(data) - 1)
-    print(data[random_index]["French"])
-    canvas.itemconfig(word, text=data[random_index]["French"])
+def random_word(data: list):
+    return random.choice(data)
     
--- main.py
+++ main.py
@@ -10,26 +10,47 @@ ROOT = Path(__file__).parent
 IMAGES = ROOT.joinpath("images")
 DATA_PATH = ROOT.joinpath("data") / "french_words.csv"
 
-# read data
+timer_id = None
+
 data = pd.read_csv(DATA_PATH)
-
-# transform into list
 data_list = data.to_dict("records")
+current_card = {}
 
-# window setup
+# ---------------- Logic -------------------
+def next_card():
+    global current_card
+    current_card = utils.random_word(data_list)
+    canvas.itemconfig(card_bg, image=front)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+
+def flip_card():
+    canvas.itemconfig(card_bg, image=back)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+    window.after_cancel(timer_id)
+
+# ---------------- Window setup ----------------------
 window = tkinter.Tk()
 window.title("Flashy")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 window.resizable(False, False)
 
+timer_id = window.after(3000, flip_card)
 
-# canvas for flashcard and text
+# ---------------- Card back & front background -------------------
+back = tkinter.PhotoImage(file=IMAGES.joinpath("card_back.png"))
+front = tkinter.PhotoImage(file=IMAGES.joinpath("card_front.png"))
+
+
+# ---------------- Canvas for flashcard and text -------------------------
 canvas = tkinter.Canvas(width=800, height=526, bg=BACKGROUND_COLOR)
-card_background = tkinter.PhotoImage(file=IMAGES.joinpath("card_front.png"))
-canvas.create_image(400, 263, image=card_background)
+# card_img = tkinter.PhotoImage(file=IMAGES.joinpath("card_front.png"))
+card_bg = canvas.create_image(400, 263, image=front)
 
-language = canvas.create_text(400, 150, text="French", font=("Arial", 25, "italic"))
-word = canvas.create_text(400, 263, text="trouve", font=("Arial", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 25, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -40,10 +61,12 @@ is_not_known_img = tkinter.PhotoImage(file=IMAGES.joinpath("wrong.png"))
 wrong_button = tkinter.Button(image=is_not_known_img, highlightthickness=0, border=0, cursor="hand2")
 right_button = tkinter.Button(image=is_known_img, highlightthickness=0, border=0, cursor="hand2")
 
-wrong_button.config(command=partial(utils.random_word, data_list, canvas, word))
-right_button.config(command=partial(utils.random_word, data_list, canvas, word))
+wrong_button.config(command=next_card)
+right_button.config(command=next_card)
 
 wrong_button.grid(row=1, column=0)
 right_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
